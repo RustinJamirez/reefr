4024a85161dc9c1296bc138e06c86ebe7a81e4c6
Justin Ramirez <rustinjamirez@gmail.com>
2020-02-25 14:09:33 -0800

    add post, put, patch and delete functions

--- reefr_api/views.py
+++ reefr_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from reefr_api import serializers
+
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -14,4 +19,26 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'hello', 'an_api_view': an_api_view})
+
+    def post(self, request):
+        """Create a hello message with a name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def put(selfs, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handle deletion of an object"""
+        return Response({'method': 'DELETE'})
 

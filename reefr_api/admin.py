0db2babdf64d0400e7169ff52ce987b968b79ef9
Justin Ramirez <rustinjamirez@gmail.com>
2020-02-25 13:53:17 -0800

    hello view

--- reefr_api/admin.py
+++ reefr_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from reefr_api import models
+
+admin.site.register(models.UserProfile)
--- reefr_api/views.py
+++ reefr_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_api_view = [
+            'Users HTTP methods as function (get, post, patch, put, delete',
+            'Is similar to a traditional Django View',
+            'Does other stuff',
+            'Mapped manually to URLs'
+        ]
+
+        return Response({'message': 'hello', 'an_api_view': an_api_view})
+

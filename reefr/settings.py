01027b7657105ed53782a65a34dad2d288517374
Justin Ramirez <rustinjamirez@gmail.com>
2020-02-28 19:56:40 -0800

    change from sqlite to postgresql

--- reefr/settings.py
+++ reefr/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'reefr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reefr',
+        'USER': 'reefr',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
